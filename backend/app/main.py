3006c75821ab95abc0291bb74158a3d9274ad269
rihabsem <semrihab@email.com>
2026-07-28 10:05:21 +0200

    database

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ scheduler = BackgroundScheduler()
 async def lifespan(app: FastAPI):
     scheduler.add_job(
         run_batch,
-        trigger=CronTrigger(hour=9, minute=59, timezone='Europe/Brussels'),
+        trigger=CronTrigger(hour=10, minute=10, timezone='Europe/Brussels'),
         id="daily_coordinateur",
         replace_existing=True
     )
