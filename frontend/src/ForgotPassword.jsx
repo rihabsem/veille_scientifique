22c1ceef65005f9b4adcc881e35c35639a24ae0b
rihabsem <semrihab@email.com>
2026-07-29 15:20:49 +0200

    password

--- frontend/src/ForgotPassword.jsx
+++ frontend/src/ForgotPassword.jsx
@@ -22,13 +22,13 @@ export default function ForgotPassword() {
             setErrors({ email: "Veuillez saisir votre email.", general: "" });
             return;
         }
-        if (!isValidEmail(email)) {
-            setErrors({
-                email: "Veuillez utiliser un email valide de l'ULB (ex : prenom.nom@ulb.be).",
-                general: ""
-            });
-            return;
-        }
+        // if (!isValidEmail(email)) {
+        //     setErrors({
+        //         email: "Veuillez utiliser un email valide de l'ULB (ex : prenom.nom@ulb.be).",
+        //         general: ""
+        //     });
+        //     return;
+        // }
 
         setIsSubmitting(true);
         try {
