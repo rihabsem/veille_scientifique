aba74e101edf902c756148d489ce7d5fb04417cd
rihabsem <semrihab@email.com>
2026-08-03 15:22:54 +0200

    fix the backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://gallstone-botanical-reps.ngrok-free.dev", //this should be localhost 8000
+    baseURL: "http://34.79.141.24:8000", 
+    //this should be localhost 8000
     withCredentials: true,//added this
   headers: {//added this
     'ngrok-skip-browser-warning': 'true',//added this
